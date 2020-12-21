305eaaa5d8234a4554332afc62c7529d821b49b1
David Chong <djchong24@gmail.com>
2020-12-21 13:41:39 -0500

    sorry i just realized in class i forgot to commit it

--- Sorts.java
+++ Sorts.java
@@ -13,4 +13,34 @@ public class Sorts{
                     data[j+1] = temp; 
                 } 
     } 
+
+    public static void selectionSort(int[] data){
+        int n = data.length;
+        for (int i = 0; i < n-1; i++){ 
+            int minimum = i;
+            for (int j = i+1; j < n; j++){
+                if (data[j] < data[minimum])
+                    minimum = j;
+            }
+            int temp = data[minimum];
+            data[minimum] = data[i];
+            data[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] data){
+        { 
+            int n = data.length; 
+            for (int i = 1; i < n; ++i) { 
+                int temp = data[i]; 
+                int j = i - 1; 
+                while (j >= 0 && data[j] > temp) { 
+                    data[j + 1] = data[j]; 
+                    j = j - 1; 
+                } 
+                data[j + 1] = temp; 
+            } 
+        } 
+    }
+
 }
