112cc2817d832709e1d8c35f16525764c6b63df1
David Chong <djchong24@gmail.com>
2020-12-22 22:22:30 -0500

    Driver lookin good, random seems really freaking slow though and i dont know how to fix it.

--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 public class Driver{
   public static void main(String[]args){
     if(args.length < 2){
@@ -9,16 +10,30 @@ public class Driver{
 
 
       int[] randData = new int[Integer.parseInt(args[0])];
+      int num = Integer.parseInt(args[0]);
+      Random x = new Random();
+      int temp = 0;
+      int bruh = 0;
 
       //overhead: 1 random array generation.
       if(args.length < 3 || args[2].equals("random")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
-      }else if(args[2].equals("equal")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+	      for(int i = 0; i < num; i++){
+          randData[i] = x.nextInt();
+        }
+      }else if(args[2].equals("equal")){                                 
+	      for(int i = 0; i < num; i++){
+          randData[i] = 0;
+        }
       }else if(args[2].equals("sorted")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+	      for(int i = 0; i < num; i++){
+          randData[i] = temp;
+          temp++;
+        }
       }else if(args[2].equals("reversed")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+	      for(int i = num; i > 0; i--){
+          randData[bruh] = i;
+          bruh++;
+        }
       }
 
       if(args[1].equals("bubble")){
